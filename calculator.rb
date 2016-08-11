5af6f6f545dc967d59547bcdce2778c8bcbf8e8a
Paul Duca <paul.r.duca@gmail.com>
2016-08-11 11:15:17 -0400

    Add loop so you can continue to calculate

--- calculator.rb
+++ calculator.rb
@@ -16,15 +16,26 @@ def calculate(integer, op_string, integer2)
 end
 
 #User Interface
+system_end = false
+until system_end == true
+	puts "Welcome to our amazing calculator!"
+	puts "We will evaluate the numbers you provide using the operator you give us, just like a calculator would do"
+	puts "What operation would you like to do? we accept addition (+) subtraction (-) multiplication (*) division (/)"
+	puts "At any point when you are done calculating,type 'done'"
+	op_string = gets.chomp
+	if op_string == 'done'
+		system_end = true
+	end
+	puts "what is the first number you would like to put into your expression"
+	integer = gets.chomp.to_i
+	if integer == 'done'
+		system_end = true
+	end
+	puts "What is the secound number you would like to put into your expression"
+	integer2 = gets.chomp.to_i
+	if integer2 == 'done'
+		system_end = true
+	end
 
-puts "Welcome to our amazing calculator!"
-puts "We will evaluate the numbers you provide using the operator you give us, just like a calculator would do"
-puts "What operation would you like to do? we accept addition (+) subtraction (-) multiplication (*) division (/)"
-op_string = gets.chomp
-puts "what is the first number you would like to put into your expression"
-integer = gets.chomp.to_i
-puts "What is the secound number you would like to put into your expression"
-integer2 = gets.chomp.to_i
 
-
-puts calculate(4, '*', 3)
+puts "Your expression evaluates to #{calculate(integer, op_string, integer2)}"
